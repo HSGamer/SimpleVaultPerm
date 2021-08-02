a144d43397992fdff896988eeec6cdb306db8e0a
Huynh Tien <huynhqtienvtag@gmail.com>
2021-08-02 22:10:15 +0700

    add default permissions

--- src/main/java/me/hsgamer/simplevaultperm/Permissions.java
+++ src/main/java/me/hsgamer/simplevaultperm/Permissions.java
@@ -1,10 +1,15 @@
 package me.hsgamer.simplevaultperm;
 
+import org.bukkit.Bukkit;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
 public class Permissions {
     public static final Permission ADMIN = new Permission("simplevaultperm.admin", PermissionDefault.OP);
+
+    static {
+        Bukkit.getPluginManager().addPermission(ADMIN);
+    }
 
     private Permissions() {
         // EMPTY
