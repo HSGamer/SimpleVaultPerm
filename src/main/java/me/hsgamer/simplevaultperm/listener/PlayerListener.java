8d3f630d132650fe9d91517b291e1fd8c345b005
Huynh Tien <huynhqtienvtag@gmail.com>
2021-09-01 23:53:31 +0700

    login event

--- src/main/java/me/hsgamer/simplevaultperm/listener/PlayerListener.java
+++ src/main/java/me/hsgamer/simplevaultperm/listener/PlayerListener.java
@@ -2,8 +2,9 @@ package me.hsgamer.simplevaultperm.listener;
 
 import me.hsgamer.simplevaultperm.SimpleVaultPerm;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
@@ -13,13 +14,15 @@ public class PlayerListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        plugin.getTimedGroupConfig().addPlayer(event.getPlayer());
-        plugin.getPermissionManager().addPermissions(event.getPlayer());
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onJoin(PlayerLoginEvent event) {
+        if (event.getResult() == PlayerLoginEvent.Result.ALLOWED) {
+            plugin.getTimedGroupConfig().addPlayer(event.getPlayer());
+            plugin.getPermissionManager().addPermissions(event.getPlayer());
+        }
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onQuit(PlayerQuitEvent event) {
         plugin.getTimedGroupConfig().removePlayer(event.getPlayer());
         plugin.getPermissionManager().removePermissions(event.getPlayer());
