850becdb01bcd9df5f31309a5ad7e81d85adc235
HSGamer <huynhqtienvtag@gmail.com>
2023-03-18 00:50:53 +0700

    optimize recalculate permissions

--- src/main/java/me/hsgamer/simplevaultperm/object/SnapshotUser.java
+++ src/main/java/me/hsgamer/simplevaultperm/object/SnapshotUser.java
@@ -6,11 +6,23 @@ import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class SnapshotUser {
+    private static final Field PERMISSIONS_FIELD;
+
+    static {
+        try {
+            PERMISSIONS_FIELD = PermissionAttachment.class.getDeclaredField("permissions");
+            PERMISSIONS_FIELD.setAccessible(true);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     private final User user;
     private final List<String> groups;
     private final Map<String, Boolean> permissions;
@@ -21,21 +33,28 @@ public class SnapshotUser {
 
     public void setup() {
         synchronized (attachmentLock) {
-            if (permissionAttachment != null) {
-                permissionAttachment.remove();
+            if (permissionAttachment == null) {
+                Plugin plugin = JavaPlugin.getProvidingPlugin(getClass());
+                Player player = plugin.getServer().getPlayer(user.getUuid());
+                if (player == null) {
+                    return;
+                }
+                permissionAttachment = player.addAttachment(plugin);
             }
 
-            Plugin plugin = JavaPlugin.getProvidingPlugin(getClass());
-            Player player = plugin.getServer().getPlayer(user.getUuid());
-            if (player == null) {
+            Map<String, Boolean> attachmentPermissions;
+            try {
+                // noinspection unchecked
+                attachmentPermissions = (Map<String, Boolean>) PERMISSIONS_FIELD.get(permissionAttachment);
+            } catch (Exception e) {
+                JavaPlugin.getProvidingPlugin(getClass()).getLogger().severe("Cannot get the permissions of the attachment");
                 return;
             }
 
-            if (!permissions.isEmpty()) {
-                PermissionAttachment attachment = player.addAttachment(plugin);
-                permissions.forEach(attachment::setPermission);
-                permissionAttachment = attachment;
-            }
+            attachmentPermissions.clear();
+            attachmentPermissions.putAll(permissions);
+
+            permissionAttachment.getPermissible().recalculatePermissions();
         }
     }
 
